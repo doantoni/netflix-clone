643d4e6313016e267952adcfcbe812d75a1ddbc6
Dorian <doantoni@gmail.com>
2021-07-29 13:43:46 +0200

    downloaded logo locally

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Nav.css";
+import netflixLogo from "./imgs/Netflix-Logo.png";
 
 const Nav = () => {
   const [show, handleShow] = useState(false);
@@ -14,11 +15,7 @@ const Nav = () => {
 
   return (
     <div className={`nav ${show && "nav__black"}`}>
-      <img
-        className="nav__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/5/50/Ntrecgf.png"
-        alt="Netflix Logo"
-      />
+      <img className="nav__logo" src={netflixLogo} alt="Netflix Logo" />
 
       <img
         className="nav__avatar"
